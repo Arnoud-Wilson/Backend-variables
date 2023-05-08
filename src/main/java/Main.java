584ca15e5245526b3a0ee0185138ee18473e5009
Arnoud-Wilson <arnoud608@gmail.com>
2023-05-08 19:37:29 +0200

    finished assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         String string_voorbeeld = "Dit is een voorbeeld";
-
-
-
+        String string_hello_world = "hello world";
+        Boolean boolean_true = true;
+        Integer int_four = 4;
+        Short short_minus_eight = -8;
+        Float float_six_point_five = 6.5f;
+        Double double_minus_two_point_three = -2.3;
 
 
         // Wijzig niets aan onderstaande System.out.println statements
@@ -20,7 +23,9 @@ public class Main {
 
         // Bonus: Wijs een nieuwe waarde toe aan een bestaande variabele
 
-        System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
-    }
+        string_voorbeeld = "Dit is een aangepast voorbeeld";
 
+        System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
+
+    }
 }
